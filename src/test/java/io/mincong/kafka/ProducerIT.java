839bd7d7faccb52ce72b64b0c351e5c9ac20bdac
Mincong HUANG <mincong.h@gmail.com>
2019-09-28 17:08:09 +0200

    Assert w/ CountDownLatch
    Assert with CountDownLatch because method KafkaCluster.Usage#consumeStrings()
    executes asynchronously. In order to make the test-thread wait until the
    completion of consumer action, a CountDownLatch is defined with initial value
    1, and only count down (to 0) at the consumer thread. Therefore, assertions
    are not performed until completion or timeout.

--- src/test/java/io/mincong/kafka/ProducerIT.java
+++ src/test/java/io/mincong/kafka/ProducerIT.java
@@ -1,16 +1,27 @@
 package io.mincong.kafka;
 
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 public class ProducerIT extends AbstractKafkaIT {
 
+  private static Logger logger = LoggerFactory.getLogger(ProducerIT.class);
+
   @Test
-  public void sendRecord() {
+  public void sendRecord() throws Exception {
     Properties props = new Properties();
     props.put("bootstrap.servers", "localhost:9092");
     props.put("acks", "all");
@@ -21,10 +32,28 @@ public class ProducerIT extends AbstractKafkaIT {
     props.put("key.serializer", StringSerializer.class.getName());
     props.put("value.serializer", StringSerializer.class.getName());
 
+    CountDownLatch completion = new CountDownLatch(1);
+    Map<String, String> messages = new ConcurrentHashMap<>();
+    kafkaCluster
+        .useTo()
+        .consumeStrings(
+            "my-topic",
+            1,
+            10,
+            TimeUnit.SECONDS,
+            completion::countDown,
+            (k, v) -> {
+              messages.put(k, v);
+              return true;
+            });
+
     try (Producer<String, String> producer = new KafkaProducer<>(props)) {
-      for (int i = 0; i < 10; i++) {
-        producer.send(new ProducerRecord<>("my-topic", Integer.toString(i), Integer.toString(i)));
-      }
+      producer.send(new ProducerRecord<>("my-topic", "k", "v"));
     }
+
+    if (!completion.await(10, TimeUnit.SECONDS)) {
+      logger.warn("Timeout: consumer and/or producer did not complete normally");
+    }
+    assertThat(messages).containsExactly(entry("k", "v"));
   }
 }
